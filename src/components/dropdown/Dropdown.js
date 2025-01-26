a11f56023a387687eb9735823b03b905a0876dfc
Furinkan <furinkan9999@gmail.com>
2025-01-27 01:58:01 +0700

    created Dropdown component

---
+++ src/components/dropdown/Dropdown.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./Dropdown.module.css"; // Import CSS for styling
+
+const Dropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  const options = ["Option 1", "Option 2", "Option 3"];
+
+  return (
+    <div className="dropdown">
+      <button className="dropdown-toggle" onClick={toggleDropdown}>
+        {selectedOption || "Select an option"}
+      </button>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          {options.map((option) => (
+            <li
+              key={option}
+              className="dropdown-item"
+              onClick={() => handleOptionClick(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
